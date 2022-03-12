3eb21d0addfa0a251b1625f04fbe5b0d685c75dd
Rafael Penido <rafaelpenido66@gmail.com>
2022-03-12 15:40:37 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -9,16 +9,25 @@ function Listing() {
 
     const [pageNumber, setPageNumber] = useState(0);
 
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
+
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
             .then(response => {
                 const data = response.data as MoviePage;
-                console.log(data);
-                setPageNumber(data.number);
+                setPage(data);
             });
-    }, []);
-
-
+    }, [pageNumber]);
 
 
     return (
@@ -26,21 +35,16 @@ function Listing() {
             <Pagination />
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-4">
-                        <MovieCard />
-                    </div>
+
+                    {page.content.map(movie =>
+                    (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-4">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
+
+
 
                 </div>
             </div>
